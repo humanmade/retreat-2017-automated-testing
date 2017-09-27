d42cf572853188982136fea81f051c8ea738e730
Paul Gibbs <djpaul@gmail.com>
2017-09-27 16:47:55 +0100

    Set a page-on-front in installation script

--- .workshop/install.sh
+++ .workshop/install.sh
@@ -16,4 +16,9 @@ wp menu create "Navigation"
 wp menu location assign navigation primary
 wp menu item add-custom navigation Movies http://workshop.local/movies/
 
+# Home page.
+homepage=$(wp post create --post_type=page --post_title="Movie Schedule" --post_content="[add_movie_form]" --post_status=publish --porcelain)
+wp option update show_on_front page
+wp option update page_on_front $homepage
+
 echo "All done!"
