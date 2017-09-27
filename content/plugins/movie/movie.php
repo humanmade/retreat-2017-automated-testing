6ef99f899a9995074acfca951c348fc8b7954884
Paul Gibbs <djpaul@gmail.com>
2017-09-27 15:45:23 +0100

    Register custom content types.

--- content/plugins/movie/movie.php
+++ content/plugins/movie/movie.php
@@ -1,8 +1,32 @@
 <?php
 /**
  * Plugin Name: Movie Plugin
+ * Description: 🔆 🎥 🤹‍♂️
  * Author:      Happy Made
  * Version:     1
  * License:     wtfpl
  */
 
+// Set up content types.
+add_action( 'init', function() {
+	register_post_type( 'movie', [
+		'label'         => 'Movies',
+		'public'        => true,
+		'show_in_rest'  => true,
+
+		// Below dashboard.
+		'menu_position' => 2,
+	] );
+
+	register_taxonomy( 'rating', 'movie', [
+		'label'         => 'Movie Rating',
+		'public'        => true,
+		'show_in_rest'  => true,
+	] );
+
+	register_taxonomy( 'genre', 'movie', [
+		'label'         => 'Movie Genre',
+		'public'        => true,
+		'show_in_rest'  => true,
+	] );
+} );
