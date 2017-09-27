aab85b1ff455ebf205f3b3d23607388c9a67fd31
Paul Gibbs <djpaul@gmail.com>
2017-09-27 23:19:10 +0100

    My OCD demands the correct rating for Deadpool is shown, even though this is sample content.

--- .workshop/install.sh
+++ .workshop/install.sh
@@ -26,9 +26,9 @@ wp term create rating Universal --porcelain
 wp term create rating 12A --porcelain
 wp term create rating 12 --porcelain
 wp term create rating 15 --porcelain
-wp term create rating 18 --porcelain
+wp term create rating PG --porcelain
 wp term create rating R18 --porcelain
-pg_rating=$(wp term create rating PG --porcelain)
+movie_rating=$(wp term create rating 18 --porcelain)
 
 # Load a movie genre.
 action_genre=$(wp term create genre Action --porcelain)
@@ -37,7 +37,7 @@ wp term meta update $action_genre genre_id 28
 # Load a movie.
 movie=$(wp post create --post_status=publish --post_type=movie --post_title="Deadpool" --post_content="Deadpool tells the origin story of former Special Forces operative turned mercenary Wade Wilson, who after being subjected to a rogue experiment that leaves him with accelerated healing powers, adopts the alter ego Deadpool. Armed with his new abilities and a dark, twisted sense of humor, Deadpool hunts down the man who nearly destroyed his life." --porcelain)
 wp media import http://image.tmdb.org/t/p/w640/inVq3FRqcYIRl2la8iZikYYxFNR.jpg --post_id=$movie --featured_image
-wp post term set $movie rating $pg_rating --by=id
+wp post term set $movie rating $movie_rating --by=id
 wp post term set $movie genre $action_genre --by=id
 
 echo "All done!"
