99fef6e726dee53ed14c45fc11fc1fe3b1a04056
Paul Gibbs <djpaul@gmail.com>
2017-09-27 17:30:39 +0100

    The one in which Paul writes CSS

--- content/themes/movietheme/functions.php
+++ content/themes/movietheme/functions.php
@@ -2,7 +2,7 @@
 // Load parent theme CSS.
 add_action( 'wp_enqueue_scripts', function() {
 	wp_enqueue_style( 'twentyfifteen-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'movietheme-style', get_stylesheet_directory_uri() . '/style.css', ['twentyfifteen-style'], '1' );
+	wp_enqueue_style( 'movietheme-style', get_stylesheet_directory_uri() . '/style.css?' . time(), ['twentyfifteen-style'], '1' );
 } );
 
 add_action( 'after_setup_theme', function() {
