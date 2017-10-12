d5403276b0444485f7a4a97724d6abfa1a79b6f1
John Blackbourn <johnbillion@gmail.com>
2017-10-12 16:13:07 +0200

    Tests!

--- tests/test-movies-cpt.php
+++ tests/test-movies-cpt.php
@@ -34,10 +34,28 @@ class Movie_CPT_Test extends WP_UnitTestCase {
 	 * @param $movie_id
 	 * @param $age
 	 * @param $expected_result
+	 * @dataProvider dataSuitable
 	 */
-	public function test_is_suitable( ) {
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
-		);
+	public function test_is_suitable( $movie_id, $age, $expected_result ) {
+		$this->assertSame( $expected_result, is_suitable_for( $movie_id, $age ) );
+	}
+
+	public static function dataSuitable() {
+		$factory = new WP_UnitTest_Factory_For_Movie_CPT;
+		$movie = $factory->create( [ 'age_rating' => '18', 'post_type' => 'movie' ] );
+		return [
+			[
+				0, 21, false
+			],
+			[
+				$movie, 21, true
+			],
+			[
+				$movie, 15, false
+			],
+			[
+				$movie, 18, true
+			],
+		];
 	}
 }
