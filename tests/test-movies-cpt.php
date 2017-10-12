87897a8a8af4d3cc074cd08017e3a3535a752a3e
Peter Wilson <git-commit-by@peterwilson.cc>
2017-10-12 16:14:17 +0200

    Dasha, Peter, Parbs

--- tests/test-movies-cpt.php
+++ tests/test-movies-cpt.php
@@ -34,10 +34,52 @@ class Movie_CPT_Test extends WP_UnitTestCase {
 	 * @param $movie_id
 	 * @param $age
 	 * @param $expected_result
+	 *
+	 * @dataProvider provider_is_suitable
 	 */
-	public function test_is_suitable( ) {
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
-		);
+	public function test_is_suitable( $movie_id, $age, $expected_result ) {
+		$actual = is_suitable_for( $movie_id, $age );
+		$this->assertSame( $actual, $expected_result );
+	}
+
+	public function provider_is_suitable() {
+
+		$r_term_id = $this->factory->term->create([
+			'taxonomy' => 'rating',
+			'name'     => 'R',
+		]);
+		add_term_meta( $r_term_id, 'age_relation', 18 );
+
+
+		$this->movie_id = $this->factory->post->create([
+			'post_type' => 'movie',
+			'post_title' => 'Revenge of the nerds.',
+		]);
+
+		wp_set_object_terms( $this->movie_id, $r_term_id, 'rating' );
+
+
+		/*
+		 * 1. Movie ID
+		 * 2. Age
+		 * 3. Exepected Result
+		 */
+		return [
+			[
+				$this->movie_id,
+				18,
+				true,
+			],
+			[
+				$this->movie_id,
+				17,
+				false,
+			],
+			[
+				$this->movie_id + 1,
+				17,
+				false,
+			],
+		];
 	}
 }
