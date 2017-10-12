d5f8894914bc9c5a9d6ffb1765a5d63f2418e2bd
pdewouters <paul@hmn.md>
2017-10-12 16:13:17 +0200

    Tests for is suitable method

--- tests/test-movies-cpt.php
+++ tests/test-movies-cpt.php
@@ -9,7 +9,7 @@ class Movie_CPT_Test extends WP_UnitTestCase {
 	/**
 	 * @var WP_UnitTest_Factory_For_Movie_CPT
 	 */
-	public $movie;
+	public static $movie;
 
 	/**
 	 * A collection of movies
@@ -28,16 +28,33 @@ class Movie_CPT_Test extends WP_UnitTestCase {
 	 */
 	public function setUp() {
 		parent::setUp();
+
+
 	}
+
 
 	/**
 	 * @param $movie_id
 	 * @param $age
 	 * @param $expected_result
+	 *
+	 * @dataProvider paramTester
 	 */
-	public function test_is_suitable( ) {
-		$this->markTestIncomplete(
-			'This test has not been implemented yet.'
-		);
+	public function test_is_suitable( $id, $age, $expected ) {
+		$result = is_suitable_for( $id, $age );
+		$this->assertEquals( $expected, $result );
+	}
+
+	public function paramTester() {
+		$this->term = $this->factory->term->create( [ 'taxonomy' => 'rating' ] );
+		update_term_meta( $this->term, 'age_relation', 18 );
+		self::$movie = $this->factory->post->create();
+		wp_set_object_terms(self::$movie, $this->term, 'rating' );
+		return [
+			[0, 10, null],
+			[self::$movie, 9, false],
+			[self::$movie, 18, true],
+			[self::$movie, 20, true],
+		];
 	}
 }
